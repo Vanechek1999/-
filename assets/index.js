923b84f67a14d46a4298423f1ec38805e2acffb5
Vanechek1999 <vanya.kulai@yandex.ru>
2021-09-27 22:04:11 +0300

    new validate area

--- assets/index.js
+++ assets/index.js
@@ -3,21 +3,26 @@ const allLevel = document.querySelectorAll('.levelEducation-all li');
 
 
 btnOfLevel.addEventListener('click', function(e){
+    showAllLevel(this)
+})
+
+function showAllLevel(btn){
     let show = document.querySelector('.levelEducation-all')
     if(!show.classList.contains('levelEducation-all-show')){
         show.classList.add('levelEducation-all-show')
-        this.style.transform = 'rotate(180deg)';
+        btn.style.transform = 'rotate(180deg)';
     }else{
         show.classList.remove('levelEducation-all-show')
-        this.style.transform = 'rotate(0deg)';
+        btn.style.transform = 'rotate(0deg)';
     }  
-})
+}
 
 function changeEducation(){
     let answer = document.querySelector('.levelEducation .answer');
     let placeholder = document.querySelector('.levelEducation .placeholder')
     allLevel.forEach(level=>{
         level.addEventListener('click', function(){
+            
             answer.innerHTML = `${level.innerText}`;
             placeholder.style.cssText=`
                 font-size: 14px;
@@ -26,16 +31,90 @@ function changeEducation(){
                 top: 13.5px;
                 color: #B2B2B2;
                 transition: .3s ease-in-out
-            `
+            `;
+            showAllLevel(btnOfLevel)
         })
     })
 }
 changeEducation()
 
-let regNumb = (/d/g);
+let regNumb = (/[^0-9]/g);
 
-const date = document.querySelector('.educationEnding');
+const date = document.querySelector('.educationEnding input');
+const dateParent = document.querySelector('.educationEnding')
+
+function validateForm(elem, text, value, important){
+    elem.addEventListener('change', function(){
+        if(+this.value == 0){
+            if(!important.classList.contains('showError')){
+                important.classList.add('showError')
+                setTimeout(()=>{
+                    important.classList.remove('showError')
+                }, 1000)
+            }
+        }else if(+this.value <10 && +this.value > 0){
+            if(!value.classList.contains('showError')){
+                value.classList.add('showError')
+                setTimeout(()=>{
+                    value.classList.remove('showError')
+                }, 1000)
+            }
+        }else if(regNumb.test(this.value)){
+            if(!text.classList.contains('showError')){
+                text.classList.add('showError')
+                setTimeout(()=>{
+                    text.classList.remove('showError')
+                }, 1000)
+            }
+        }
+    })
+}
+const series = document.querySelector('.series');
+const number = document.querySelector('.number')
+let seriesImportant = document.querySelector('.importantArea');
+let seriesCount = document.querySelector('.countOfSymbols')
+let seriesValidate =document.querySelector('.validateMessage')
+validateForm(series, seriesImportant, seriesCount, seriesValidate)
+
+date.addEventListener('input', function(){
+    let error = document.querySelector('.educationEnding span')
+    if(regNumb.test(this.value)){
+        if(!error.classList.contains('showError')){
+            error.classList.add('showError')
+            setTimeout(()=>{
+                error.classList.remove('showError')
+            }, 1000)
+        }
+    }
+})
+
+
 function validate(evt){
     evt.value = evt.value.replace(/[^0-9]/g,"");
 
 }
+const nameUnivercity = document.querySelector('.name input');
+const allUnivercity = document.querySelector('.allUnivercity');
+const clear = document.querySelector('.name .close')
+const closeWindow = document.querySelector('.closeWindow')
+nameUnivercity.addEventListener('input', function(){
+    if(this.value !== ''){
+        clear.classList.add('showCloseIcon')
+    }else{
+        clear.classList.remove('showCloseIcon')
+    }
+})
+
+clear.addEventListener('click', function(){
+    nameUnivercity.value = '';
+    this.classList.remove('showCloseIcon')
+})
+closeWindow.addEventListener('click', function(){
+    if(!allUnivercity.classList.contains('showAllUnivercity')){
+        allUnivercity.classList.add('showAllUnivercity');
+        this.style.transform = 'rotate(180deg)'
+    }else{
+        allUnivercity.classList.remove('showAllUnivercity');
+        this.style.transform = 'rotate(0deg)'
+    }
+})
